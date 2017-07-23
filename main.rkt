867a375ecc3347b6a892652c6522a217d1697a9b
Jack Firth <jackhfirth@gmail.com>
2017-07-22 19:39:43 -0700

    Move tests to separate module for coverage purposes

--- main.rkt
+++ main.rkt
@@ -59,36 +59,3 @@
        (with-test-fixture fixture.id
          (test-begin/fixture rest.unsplice ... ... body ...)))]
   [(_ body:expr ...+) #'(test-begin body ...)])
-
-(module+ test
-  (test-case "test-begin/fixture"
-    (define-values (seq log)
-      (disposable/event-log (sequence->disposable '(1 2 3))))
-    (define item (fixture seq))
-    (test-begin/fixture
-      #:fixture item
-      (check-equal? (item) 1)
-      (test-case "first-nested" (check-equal? (item) 2))
-      (test-case "second-nested" (check-equal? (item) 3))
-      (check-equal? (item) 1))
-    (define expected-log
-      '((alloc 1) (alloc 2) (dealloc 2) (alloc 3) (dealloc 3) (dealloc 1)))
-    (check-equal? (log) expected-log))
-  (test-case "test-begin/fixture #:as"
-    (test-begin/fixture
-      #:fixture (fixture (disposable-pure 'foo)) #:as foo
-      (check-equal? (foo) 'foo)))
-  (test-case "test-begin/fixture multiple"
-    (define foo-fix (fixture (disposable-pure 'foo)))
-    (test-begin/fixture
-      #:fixture foo-fix
-      #:fixture (fixture (disposable-pure 'bar)) #:as bar-fix
-      (check-equal? (foo-fix) 'foo)
-      (check-equal? (bar-fix) 'bar)))
-  (define foo-fix (fixture (disposable-pure 'foo)))
-  (test-case/fixture "test-case/fixture"
-    #:fixture foo-fix
-    #:fixture (fixture (disposable-pure 'bar)) #:as bar-fix
-    (check-equal? (foo-fix) 'foo)
-    (check-equal? (bar-fix) 'bar)
-    (check-equal? (current-test-name) "test-case/fixture")))
