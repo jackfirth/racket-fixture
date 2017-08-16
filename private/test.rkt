5efcd377b8d6d93dd546363e5e83991643687c6c
Jack Firth <jackhfirth@gmail.com>
2017-08-15 21:14:30 -0700

    Fix tests, for real this time

--- private/test.rkt
+++ private/test.rkt
@@ -11,20 +11,22 @@
            rackunit))
 
 (module+ test
+  (define item/log (disposable/event-log (sequence->disposable '(1 2 3))))
+  
   (test-case "test-begin/fixture"
-    (define-fixture item+log
-      (disposable/event-log (sequence->disposable '(1 2 3))))
-    (define (item) (first (item+log)))
-    (define (item-evts) (event-log-events (second (item+log))))
-    (test-begin/fixture
-      #:fixture item+log
-      (check-equal? (item) 1)
-      (test-case "first-nested" (check-equal? (item) 2))
-      (test-case "second-nested" (check-equal? (item) 3))
-      (check-equal? (item) 1)
-      (define expected-log
-        '((alloc 1) (alloc 2) (dealloc 2) (alloc 3) (dealloc 3)))
-      (check-equal? (item-evts) expected-log)))
+    (with-disposable ([item+log item/log])
+      (define-fixture item (first item+log))
+      (define (item-evts) (event-log-events (second item+log)))
+      (test-begin/fixture
+        #:fixture item
+        (check-equal? (item) 1)
+        (test-case "first-nested" (check-equal? (item) 2))
+        (test-case "second-nested" (check-equal? (item) 3))
+        (check-equal? (item) 1)
+        (define expected-log
+          '((alloc 1) (alloc 2) (dealloc 2) (alloc 3) (dealloc 3)))
+        (check-equal? (item-evts) expected-log))))
+  
   (test-case "test-begin/fixture multiple"
     (define-fixture foo-fix (disposable-pure 'foo))
     (define-fixture bar-fix (disposable-pure 'bar))
@@ -33,11 +35,13 @@
       #:fixture bar-fix
       (check-equal? (foo-fix) 'foo)
       (check-equal? (bar-fix) 'bar)))
+  
   (define-fixture foo-fix (disposable-pure 'foo))
   (test-case/fixture "test-case/fixture"
     #:fixture foo-fix
     (check-equal? (foo-fix) 'foo)
     (check-equal? (current-test-name) "test-case/fixture"))
+  
   (test-case "documentation"
     (check-all-documented 'fixture)
     (check-all-documented 'fixture/base)
