4ab3b1d067d58af2b63beb04c71c55406d210e98
Jack Firth <jackhfirth@gmail.com>
2017-09-03 16:55:48 -0700

    Fix how fixture info is added

--- rackunit.rkt
+++ rackunit.rkt
@@ -7,6 +7,7 @@
 
 (require (for-syntax racket/base)
          racket/function
+         racket/list
          rackunit
          syntax/parse/define
          "base.rkt")
@@ -15,31 +16,52 @@
 (define (fixture->check-info fix)
   (make-check-info (fixture-name fix) (fixture-info fix)))
 
-(define (call/infos fixs thnk)
-  (with-check-info (['fixtures (nested-info (map fixture->check-info fixs))])
+(define current-test-fixtures (make-parameter '()))
+
+(define (call/test-fixtures fixs thnk)
+  (parameterize ([current-test-fixtures (append (current-test-fixtures) fixs)])
     (thnk)))
 
-(define (call/fixtures fixs thnk)
-  ((for/fold ([thnk thnk])
-             ([fix (in-list fixs)])
-     (thunk (call/fixture fix thnk)))))
+(define (compute-infos)
+  (nested-info (map fixture->check-info (current-test-fixtures))))
 
-(define (around/fixtures fixs thnk)
-  (call/fixtures fixs (thunk (call/infos fixs thnk))))
+(define (call/test-fixtures-info thnk)
+  (if (empty? (current-test-fixtures))
+      (with-check-info (['fixture (dynamic-info compute-infos)]) (thnk))
+      (thnk)))
 
-(define (call/test-case-around/fixtures fixs thnk)
-  (define old-around (current-test-case-around))
-  (define (around thnk) (old-around (thunk (around/fixtures fixs thnk))))
-  (parameterize ([current-test-case-around around]) (thnk)))
-  
+(define (test-case-around/fixtures old-around fixs thnk)
+  (old-around
+   (thunk
+    ((for/fold ([thnk thnk])
+               ([fix (in-list (current-test-fixtures))])
+       (thunk (call/fixture fix thnk)))))))
+
 (define-simple-macro
   (test-begin/fixture (~seq (~seq #:fixture fixture:expr) ...) body:expr ...+)
-  (call/test-case-around/fixtures (list fixture ...)
-                                  (thunk (test-begin body ...))))
+  #:with (fix-id ...) (generate-temporaries #'(fixture ...))
+  (let* ([fix-id fixture] ...
+         [all-fixs (list fix-id ...)]
+         [old-around (current-test-case-around)])
+    (call/test-fixtures-info
+     (thunk
+      (call/test-fixtures
+       all-fixs (thunk
+                 (test-case-around/fixtures
+                  old-around all-fixs (thunk (test-begin body ...)))))))))
+
 
 (define-simple-macro
   (test-case/fixture name:str
     (~seq (~seq #:fixture fixture:expr) ...)
     body:expr ...+)
-  (call/test-case-around/fixtures (list fixture ...)
-                                  (thunk (test-case name body ...))))
+  #:with (fix-id ...) (generate-temporaries #'(fixture ...))
+  (let* ([fix-id fixture] ...
+         [all-fixs (list fix-id ...)]
+         [old-around (current-test-case-around)])
+    (call/test-fixtures-info
+     (thunk
+      (call/test-fixtures
+       all-fixs (thunk
+                 (test-case-around/fixtures
+                  old-around all-fixs (thunk (test-case name body ...)))))))))
